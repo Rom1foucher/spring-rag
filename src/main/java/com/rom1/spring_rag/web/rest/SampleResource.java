a2bd07f8db8430a91ed8bc708ff4464db41ec5e7
Romain Foucher <romain.foucher@efrei.net>
2025-01-08 15:48:45 +0100

    #7 create sample controler

---
+++ src/main/java/com/rom1/spring_rag/web/rest/SampleResource.java
@@ -0,0 +1,12 @@
+package com.rom1.spring_rag.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class SampleResource {
+
+    @GetMapping("/samples/{value}")
+    public String hello(@PathVariable(value = "value", required = false) final String value) {
+        return "Hello " + value + "!";
+    }
+}
