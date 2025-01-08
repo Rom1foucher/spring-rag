0f59c8c348191ef1a357d14b9350804e27beac97
Romain Foucher <romain.foucher@efrei.net>
2025-01-08 14:09:41 +0100

    #1 create project

---
+++ src/test/java/com/rom1/spring_rag/SpringRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rom1.spring_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
